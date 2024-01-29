ee66e7ca901aa634e6ae868d1b080bed4d888176
anthony <ndegwaanthony300@gmail.com>
2024-01-29 09:55:24 +0000

    0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    /* Allocate memory for the new node */
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+    /* Check if memory allocation was successful */
+    if (new_node == NULL) {
+        return NULL; // Return NULL on failure
+    }
+
+    /* Initialize the fields of the new node */
+    new_node->n = value;
+    new_node->parent = parent;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    return new_node;
+}
