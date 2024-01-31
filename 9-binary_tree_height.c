380a3256923bb6ebcf5e18e732d1a7070f1cf606
Anthony <ndegwaanthony300@gmail.com>
2024-01-31 23:16:03 +0300

    9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,18 +8,18 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t left_height, right_height;
+	size_t left_height, right_height;
 
-    /* Base case: If tree is NULL, return 0 */
-    if (tree == NULL)
-        return (0);
+	/* Base case: If tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
 
-    /* Recursive call to find the height of the left subtree */
-    left_height = binary_tree_height(tree->left);
+	/* Recursive call to find the height of the left subtree */
+	left_height = binary_tree_height(tree->left);
 
-    /* Recursive call to find the height of the right subtree */
-    right_height = binary_tree_height(tree->right);
+	/* Recursive call to find the height of the right subtree */
+	right_height = binary_tree_height(tree->right);
 
-    /* Return the maximum height between left and right subtrees, plus 1 for the current level */
-    return (1 + (left_height > right_height ? left_height : right_height));
+	/* Return max height btn left&right subtrees,+1 for current level */
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
