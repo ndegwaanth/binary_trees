8bdcc2d4b76f0e313b042cdf3293cdd1d7e82991
Anthony <ndegwaanthony300@gmail.com>
2024-02-01 00:28:10 +0300

    13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -8,13 +8,13 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    /* Base case: If tree is NULL, return 0 */
-    if (tree == NULL)
-        return (0);
+	/* Base case: If tree is NULL, return 0 */
+	if (tree == NULL)
+		return (0);
 
-    /* If it has at least 1 child, return 1 + the nodes of left and right subtrees */
-    if (tree->left != NULL || tree->right != NULL)
-        return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-    else
-        return (0);
+	/* If child>1, return 1 + the nodes of left and right subtrees */
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	else
+		return (0);
 }
