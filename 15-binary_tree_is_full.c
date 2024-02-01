8763b96fca472ee7753967226057f9ef73a2192d
Anthony <ndegwaanthony300@gmail.com>
2024-02-01 07:05:38 +0300

    15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,14 +8,14 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-    /* Base case: If tree is NULL, it is considered full */
-    if (tree == NULL)
-        return (1);
+	/* Base case: If tree is NULL, it is considered full */
+	if (tree == NULL)
+		return (1);
 
-    /* If one of the subtrees is NULL and the other is not, the tree is not full */
-    if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
-        return (0);
+	/* If one of the subtrees is NULL&other isn't, the tree is not full */
+	if ((tree->left == NULL && tree->right != NULL) || (tree->left != NULL && tree->right == NULL))
+		return (0);
 
-    /* Recursive check for fullness in left and right subtrees */
-    return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+	/* Recursive check for fullness in left and right subtrees */
+	return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
